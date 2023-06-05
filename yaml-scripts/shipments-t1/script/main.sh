ab8b88af52a573e34bf0d719f7eb32685f1a8e0a
Matheus Tanaka <matheustanakanog@gmail.com>
2023-06-05 13:03:30 -0300

    Create CLI function

--- yaml-scripts/shipments-t1/script/main.sh
+++ yaml-scripts/shipments-t1/script/main.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
-source ./bin/mapFolder.sh
-
+source ./bin/addNewField.sh
+source ./bin/mapDirWithoutDeploy.sh
+source ./bin/listTolerationFiles.sh
 
 main() {
-    mapFolder
+
+echo "Selecione um número para executar uma funcão"
+
+actions=("Adicionar nova TAG" "Mapear arquivos sem o arquivo de Deploy" "Listar arquivos com a TAG Toleration" "Sair")
+
+local continueLoop=true
+
+while $continueLoop; do  
+    select item in "${actions[@]}"
+    do
+        case $REPLY in
+            1) echo "Item #$REPLY selecionado - $item";
+                addNewField
+                break;;
+            2) echo "Item #$REPLY selecionado - $item";
+                dirWithoutDeploy 
+                break;;
+            3) echo "Item #$REPLY selecionado - $item"; 
+                listTolerationFiles
+                break;;
+            4) echo "Item #$REPLY selecionado - $item";
+                continueLoop=false
+                break;; 
+        esac
+    done
+done
 }
 
 main
